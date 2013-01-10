31afa2c93b65ac211fdae5ebec833ad2cbbc710f
Anis Khan <aniskhan25@gmail.com>
2013-01-10 13:21:54 +0100

    v130110

--- demo.m
+++ demo.m
@@ -18,10 +18,21 @@ addpath(fullfile(path, 'commun'));
 
 addpath(path);
 
+if( ~exist('data/output/static','dir')), mkdir('data/output/static'); end;
+if( ~exist('data/output/dynamic','dir')), mkdir('data/output/dynamic'); end;
+if( ~exist('data/output/face','dir')), mkdir('data/output/face'); end;
+if( ~exist('data/output/fusion','dir')), mkdir('data/output/fusion'); end;
+
 %% COMPILE mex functions
 
-mex voie_visage/src/FaceDetect.cpp -Ivoie_visage/inc/ voie_visage/lib/*.lib -outdir voie_visage/bin/
-mex voie_visage/src/FaceDetectStrict.cpp -Ivoie_visage/inc/ voie_visage/lib/*.lib -outdir voie_visage/bin/
+disp('COMPILE mex functions.');
+
+if( ~exist('voie_visage/bin/FaceDetect','file')),
+    mex voie_visage/src/FaceDetect.cpp -Ivoie_visage/inc/ voie_visage/lib/*.lib -outdir voie_visage/bin/
+end;
+if( ~exist('voie_visage/bin/FaceDetectStrict','file')),
+    mex voie_visage/src/FaceDetectStrict.cpp -Ivoie_visage/inc/ voie_visage/lib/*.lib -outdir voie_visage/bin/
+end;
 
 %% SET IO variables
 
@@ -33,42 +44,63 @@ comppath = [fullfile(pathstr,name) '.txt'];
 
 %% GET video file
 
+disp('READ input video.');
+
 video = mmreader(ipath);
 
 nframes = video.NumberOfFrames;
 
 %% GET camera motion compensation
 
-estim_mvt_dominant(ipath,comppath);
+disp('COMPUTE camera motion variables.');
+    
+if( ~exist(comppath,'file')),
+    estim_mvt_dominant(ipath,comppath);
+end;
 
 %% LOOP thru each frame
 
-for i=1:nframes-1,
+disp('COMPUTE saliency ...');
+
+%for i=1:nframes-1,
+for i=1:3, % first three frames
+
+    disp(['Frame ' num2str(i+1) '...']);
     
     %% READ frames
     
     im1 = rgb2gray(im2double(read(video,i)));
     im2 = rgb2gray(im2double(read(video,i+1)));
     
+    %% COMPUTE static saliency map
+    
+    disp('COMPUTE static saliency map.');
+    
+    S = saillance_statique(im2);
+    
     %% COMPUTE dynamic saliency map
+    
+    disp('COMPUTE dynamic saliency map.');
     
     [D,D_brut] = saillance_dynamique( ...
         im1, im2 ...
         , 0, comppath, 0, 1 );
     
-    %% COMPUTE static saliency map
-    
-    S = saillance_statique(im2);
-    
     %% COMPUTE face saliency map
+    
+    disp('COMPUTE face saliency map.');
     
     [F,faces] = saillance_visage(im2);
     
     %% COMPUTE master/fused saliency map
     
-    M = MTI_compute_fusion_simple(S,D_brut,F,faces);
+    disp('COMPUTE fusion/master saliency map.');
     
+    M = MTI_compute_fusion_simple(S,D_brut,F,faces);
+        
     %% WRITE saliency maps
+
+    disp('SAVE saliency maps.');
     
     imwrite(S,[fullfile(opath,'static',name)  '_' num2str(i) '_sta.png'],'png');
     imwrite(D,[fullfile(opath,'dynamic',name) '_' num2str(i) '_dyn.png'],'png');
@@ -76,3 +108,5 @@ for i=1:nframes-1,
     imwrite(M,[fullfile(opath,'fusion',name)  '_' num2str(i) '_fus.png'],'png');
     
 end
+
+disp('COMPUTE saliency completed.');
--- mvt_dominant/estim_mvt_dominant.m
+++ mvt_dominant/estim_mvt_dominant.m
@@ -2,14 +2,10 @@ function estim_mvt_dominant(vpath,tpath)
 
 cd mvt_dominant/prog
 
-l_vpath = ['..\..\' vpath];
+l_vpath = strrep (['..\..\' vpath], '/', filesep);
 l_tpath = ['..\..\' tpath];
 
-chaine=sprintf('Motion2D -m AC -p %s -f 0 -r %s -v',l_vpath,l_tpath);
+chaine=sprintf('Motion2D -m AC -p %s -f 0 -r %s',l_vpath,l_tpath);
 dos(chaine);%AC ou QC et support -a ./support/support -c 255 -w erreur
 
 cd ../..
-
-
-
-
